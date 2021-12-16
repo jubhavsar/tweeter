594df66df44ff37742e3c8cb040bc69ce2a490ef
jubhavsar <juhi_bhavsar@yahoo.com>
2021-12-16 13:22:41 -0500

    implement form validation condition and XSS

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,59 +1,45 @@
-$(document).ready(function() {
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: timeago.format(1461116232227),
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: timeago.format(1461113959088),
-  },
-];
-// get whole array of data
-const renderTweets = function (tweets) {
-  // loops through tweets
-  for (let tweet of tweets) {
-    // calls createTweetElement for each tweet
-    const $tweet = createTweetElement(tweet);
-    console.log($tweet);
-    // takes return value and appends it to the tweets container
-    $("#tweets-container").append($tweet);
-  }
-};
-// create createTweetElement(Markup)   
-const createTweetElement = function (tweet) {
-  const $tweet = $(`
+$(document).ready(function () {
+  // escape function for Cross-Site Scripting
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  // get whole array of data
+  const renderTweets = function (tweets) {
+    const $tweetsContainer = $("#tweets-container");
+    $tweetsContainer.empty();
+
+    // loops through tweets
+    for (let tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      console.log($tweet);
+      // takes return value and appends it to the tweets container
+      $("#tweets-container").prepend($tweet);
+    }
+  };
+  // create createTweetElement(Markup)
+  const createTweetElement = function (tweet) {
+    const $tweet = $(`
       <div class="space-div">
 
       </div>
     <article class="my-article">
       <header class="my-header">
         <div>
-          <img ${tweet.user.avatars}/>
-          <span> ${tweet.user.name} </span>
+          <img ${escape(tweet.user.avatars)}/>
+          <span> ${escape(tweet.user.name)} </span>
         </div>
           <span class="email-color"> ${tweet.user.handle} </span>
         </header>
         <div>
-          <p> ${tweet.content.text} </p>
+          <p> ${escape(tweet.content.text)} </p>
           <hr>
         </div>
         <footer class="my-header">
-          <span> ${tweet.created_at} </span>
+          <span> ${timeago.format(escape(tweet.created_at))} </span>
           <div class="email-color">
             <i class="fa-solid fa-heart"></i>
             <i class="fa-solid fa-flag"></i>
@@ -64,46 +50,44 @@ const createTweetElement = function (tweet) {
     </article>
         
          `);
-  return $tweet;
-};
+    return $tweet;
+  };
 
-// Using AJAX fetch (GET) data from the server.
-const fetchTweets = () => {
-      $.ajax({
-        url: '/tweets',
-        method: 'GET',
-        dataType: 'json',
-        success: (tweets) => {
-          console.log("data", tweets);
-          renderTweets(tweets);
-        },
-        error: (err) => {
-          console.log(`error: ${err}`)
-        } 
-      });
-    };
-  
-fetchTweets();
+  // Using AJAX fetch (GET) data from the server.
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: "json",
+      success: (tweets) => {
+        //console.log("data", tweets);
+        renderTweets(tweets);
+      },
+      error: (err) => {
+        console.log(`error: ${err}`);
+      },
+    });
+  };
 
-// Using AJAX send data to the server
-const $form = $('#new-tweet-form');
-console.log($form);
-$form.on('submit', function(event){
-  event.preventDefault();
-  console.log('The form was submitted!')
-  const serializedData = $(this).serialize();
+  loadTweets();
 
-  console.log(serializedData);
-
-  $.ajax({
-    url : "/tweets",
-    method : "POST",
-    data : serializedData
-  }).then(res =>{
-      console.log(res);
-  })  
-
-  })
-  //renderTweets(data);
+  // Using AJAX send data to the server
+  const $form = $("#new-tweet-form");
+  $form.on("submit", function (event) {
+    event.preventDefault();
+    console.log("The form was submitted!");
+    const serializedData = $(this).serialize();
+    const text = serializedData.replace("text=", "");
+    if (text.length > 140) {
+      alert("Tweet should not be empty and more than 140 char");
+      return;
+    }
+    if (text === null || text === "") {
+      alert("Tweet cannot be empty.please enter data");
+      return;
+    }
+    $.post("/tweets", serializedData, (response) => {
+      loadTweets();
+    });
+  });
 });
-
