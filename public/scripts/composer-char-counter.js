cbfcc3d68289624cf4b9b4a119a880070ad17e9d
jubhavsar <juhi_bhavsar@yahoo.com>
2021-12-16 13:20:59 -0500

    compose character counter using input event instead of keypress

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,21 @@
-$(document).ready(function() {
-    // add event listener to the textarea.
-   $("#tweet-text").keypress(function(e){
+$(document).ready(function () {
+  // add event listener to the textarea.
+  $("#tweet-text").on("input", function (e) {
+    
     // get the value user typed in.
     const text = $("#tweet-text").val();
-    // get total length of text
-        const totalChar = e.target.value.length;
-    //const totalChar = $("#my-output").val(text.length);
-    const output = 140 - totalChar;
-    $('#my-output').text(output);
-    if(output>0){
-      $('#my-output').removeClass("red-counter");
-    }
-     if(output<0){
-      $('#my-output').addClass("red-counter");
 
+    // get total length of text
+    const totalChar = e.target.value.length;
+
+    const output = 140 - totalChar;
+    $("#my-output").text(output);
+    if (output > 0) {
+      $("#my-output").removeClass("red-counter");
+    }
+
+    if (output < 0) {
+      $("#my-output").addClass("red-counter");
     }
   });
- 
 });
