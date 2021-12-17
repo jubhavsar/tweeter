a7755a7215f334e301c6af29e5ebdae165b6cdbb
jubhavsar <juhi_bhavsar@yahoo.com>
2021-12-16 20:56:24 -0500

    reset form using jQuery reset() function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-  // escape function for Cross-Site Scripting
-  const escape = function (str) {
+
+  // escape function for Cross-Site Scripting(XSS)
+    const escape = function (str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
   // get whole array of data
-  const renderTweets = function (tweets) {
+    const renderTweets = function (tweets) {
     const $tweetsContainer = $("#tweets-container");
     $tweetsContainer.empty();
 
     // loops through tweets
-    for (let tweet of tweets) {
+     for (let tweet of tweets) {
       // calls createTweetElement for each tweet
       const $tweet = createTweetElement(tweet);
       console.log($tweet);
@@ -20,12 +21,10 @@ $(document).ready(function () {
       $("#tweets-container").prepend($tweet);
     }
   };
-  // create createTweetElement(Markup)
-  const createTweetElement = function (tweet) {
-    const $tweet = $(`
-      <div class="space-div">
 
-      </div>
+  // create createTweetElement(Markup) using template literal.
+    const createTweetElement = function (tweet) {
+    const $tweet = $(`
     <article class="my-article">
       <header class="my-header">
         <div>
@@ -33,22 +32,20 @@ $(document).ready(function () {
           <span> ${escape(tweet.user.name)} </span>
         </div>
           <span class="email-color"> ${tweet.user.handle} </span>
-        </header>
+      </header>
         <div>
           <p> ${escape(tweet.content.text)} </p>
           <hr>
         </div>
-        <footer class="my-header">
+      <footer class="my-header">
           <span> ${timeago.format(escape(tweet.created_at))} </span>
           <div class="email-color">
             <i class="fa-solid fa-heart"></i>
             <i class="fa-solid fa-flag"></i>
             <i class="fas fa-retweet"></i>
-
           </div>
-        </footer>
-    </article>
-        
+      </footer>
+    </article>     
          `);
     return $tweet;
   };
@@ -68,29 +65,29 @@ $(document).ready(function () {
       },
     });
   };
-
   loadTweets();
 
   // Using AJAX send data to the server
   const $form = $("#new-tweet-form");
   $form.on("submit", function (event) {
-    event.preventDefault();
-    console.log("The form was submitted!");
-    const serializedData = $(this).serialize();
-    const text = serializedData.replace("text=", "");
+  event.preventDefault();
+
+  const serializedData = $(this).serialize();
+  const text = serializedData.replace("text=", "");
+
     if (text.length > 140) {
       $("#alert-msg").show();
-
-      //alert("Tweet should not be empty and more than 140 char");
       return;
     }
+
     if (text === null || text === "") {
       $("#alert-msg").show();
-      //alert("Tweet cannot be empty.please enter data");
       return;
     }
+    
     $.post("/tweets", serializedData, (response) => {
       loadTweets();
+      $('#new-tweet-form')[0].reset();
     });
   });
 });
